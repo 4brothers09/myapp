ae0812941b1efc0f30d0b3f1cc6b5b18e966897f
YOUR NAMEReaper <YOUR@EMAIL.com4brothers09@gmail.com>
2018-07-29 16:28:03 +0800

    how are you

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "hello, world!"
+  	render html: "hello, Israel how are yous!"
   end
 end
