314ad425d9854506d6741be941f5f5677c97a27a
YOUR NAMEReaper <YOUR@EMAIL.com4brothers09@gmail.com>
2018-08-27 17:23:01 +0800

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
-.
-.
-.
+require_relative '../config/enviroment'
+require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   fixtures :all
-  include ApplicationHelper
-  .
-  .
-  .
+
+   def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
