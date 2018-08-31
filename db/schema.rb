a05dfe63093d2eb879c8498c11041f16c1cf0424
YOUR NAMEReaper <YOUR@EMAIL.com4brothers09@gmail.com>
2018-08-31 16:15:01 +0800

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_110746) do
+ActiveRecord::Schema.define(version: 2018_08_31_075434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2018_08_11_110746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin"
   end
 
 end
