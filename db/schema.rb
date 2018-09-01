730e6378264c103f95c6315c5c61945b960bb61f
YOUR NAMEReaper <YOUR@EMAIL.com4brothers09@gmail.com>
2018-09-01 11:59:11 +0800

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_082711) do
+ActiveRecord::Schema.define(version: 2018_09_01_030209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2018_08_31_082711) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
